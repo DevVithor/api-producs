11e33c147b2f182d3124276a00175571d879a5c9
DevVithor <DevVithor@gmail.com>
2023-11-21 22:16:31 -0300

    feat: add test for find product sales

--- src/controllers/sale-controller.js
+++ src/controllers/sale-controller.js
@@ -1,6 +1,11 @@
+const client = require('../service/prisma');
+
 module.exports = {
   async index(req, res) {
     // trazer todas as vendes
+    const sales = await client.sale.findMany({});
+
+    return res.status(200).json(sales);
   },
   async create(req, res) {},
   async update(req, res) {},
