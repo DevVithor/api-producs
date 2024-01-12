9f6d546726df9ffbdb400c83b9e009e0079ad321
DevVithor <DevVithor@gmail.com>
2024-01-11 21:47:57 -0300

    feat: style: removed comments into code

--- src/controllers/sale-controller.js
+++ src/controllers/sale-controller.js
@@ -2,7 +2,6 @@ const client = require('../service/prisma');
 
 module.exports = {
   async index(req, res) {
-    // trazer todas as vendes
     const sales = await client.sale.findMany({});
 
     return res.status(200).json(sales);
