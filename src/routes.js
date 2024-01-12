448eea4e01c5d677be0ffe7ba6ba589a88d34820
DevVithor <DevVithor@gmail.com>
2024-01-11 21:46:15 -0300

    feat: style: removed comments into code

--- src/routes.js
+++ src/routes.js
@@ -5,20 +5,15 @@ const saleController = require('./controllers/sale-controller');
 
 const router = Router();
 
-/**
- * Metodos HTTP GET POST PUT PATCH e DELETE
- */
-
-// Get Buscar
 router.get('/sale', saleController.index);
 router.get('/products', productsController.index);
 router.get('/product/:id', productsController.findById);
-// Post Criar
+
 router.post('/product', productsController.create);
-// Put ou Patch atualizar
+
 router.put('/product/:id', productsController.updateAll);
 router.patch('/product/:id', productsController.updatePrice);
-// Delete deletar
+
 router.delete('/product/:id', productsController.delete);
 
 module.exports = router;
